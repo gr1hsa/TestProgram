da14dc26a9a9c62a501beb7f321a2447578b25ec
Gregory <doggrig@gmail.com>
2020-12-25 13:10:24 +0300

    Всё удолил

--- TestProgram/Program.cs
+++ TestProgram/Program.cs
@@ -7,11 +7,7 @@ namespace TestProgram
         static void Main(string[] args)
         {
             Console.WriteLine(2 + 4);
-            Console.WriteLine(2 * 4);
-            Console.WriteLine(2 / 4);
-            Console.WriteLine(2 ^ 4);
-            Console.WriteLine(7 - 2);
-            Console.WriteLine("Привет");
+            
         }
     }
 }
